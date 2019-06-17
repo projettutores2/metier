3d2b3962f3932602278e8ecb969cd1884ede4007
Quadrapus <45936338+Quadrapus@users.noreply.github.com>
2019-06-17 16:20:56 +0200

    Add files via upload

--- Metier.java
+++ Metier.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import iut.algo.*;
+
 public class Metier
 {
 	private Controleur         ctrl ;
@@ -25,22 +27,45 @@ public class Metier
 
 	public void jouer()
 	{
-		String algorithme = "";
+		/*String algorithme = "";
 		for(Joueur joueur : joueurs)
 		{
 			if(!this.end)
 			{
+				int i = 0;
 				System.out.println("Algorithme Robot 1 : ");
-				for(Ordre ordre : joueur.getRobot(0).getOrdre())
-					algorithme += "\n\t"  + ordre.toString();
-				System.out.println(algorithme + "\nAlgorithme Robot 2 : ");
-				algorithme = "";
-				for(Ordre ordre : joueur.getRobot(1).getOrdre())
-					algorithme += "\n\t"  + ordre.toString();
+				for(Ordre ordre : joueur.getRobot(0).getAlgo())
+					System.out.println("\t" + (++i) + ordre.toString());
+				System.out.println("Algorithme Robot 2 : ");
+				for(Ordre ordre : joueur.getRobot(1).getAlgo())
+					System.out.println("\t" + (++i) + ordre.toString());
+				System.out.println("Quelle action faire ? \n"                     + 
+									"1 - Placer un ordre \n"                     +
+									"2 - Permuter deux ordres \n"                 +
+									"3 - Retirer un ordre \n"                    +
+									"4 - Retirer tous les ordres d'un robot \n" +
+									"5 - Ne rien faire \n"                        );
+									
+				int choix = Clavier.lire_int();
 				
+				if(choix<1 || choix>5) choix=5;
 				
+				switch (choix)
+				{
+					case 1:
+						System.out.println("Quel ordre ajouter ?");
+						for(Ordre ordre : joueur.getStockOrdres())
+							System.out.println("\t" + (++i) + ordre.toString());
+						int nouvelOrdre = Clavier.lire_int();
+						System.out.println("Dans quel slot voulez vous l'ajouter ?");
+						int slot    = Clavier.lire_int();
+						joueur.getRobot((slot < 3 ? 0 : 1)).getOrdre(slot%3);
+						if(
+						break;
+				}
 			}
 		}
+		*/
 	}
 
 	//--------------------------------------------------------------
