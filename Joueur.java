a174aa2ae7f6b1d8073eacc2f4a4c64b1adb0a29
Quadrapus <45936338+Quadrapus@users.noreply.github.com>
2019-06-17 14:51:44 +0200

    Add files via upload

--- Joueur.java
+++ Joueur.java
@@ -1,12 +1,40 @@
+import java.util.list;
+
 public class Joueur
 {
-	public Joueur(String s)
+	private Metier metier;
+	private String nom;
+	private ArrayList<Ordre> stockOrdres;
+	private Robot[] ensRobots;
+	private int nbPoint;
+	
+	public Joueur(String nom, Metier metier)
 	{
-
+		this.nom         = nom;
+		this.metier      = metier;
+		this.stockOrdres = new ArrayList<Ordre>();
+		this.ensRobots   = new Robot[2];
+		this.nbPoint     = 0;
 	}
 
-	public void creeRobot(int nombre)
+	public void creeRobot(Robot robot1, Robot robot2)
 	{
-		
+		this.ensRobots[0] = robot1;
+		this.ensRobots[1] = robot2;
+	}
+	
+	public void setPosRobot(int x, int y, int z, int indRobot)
+	{
+		this.ensRobots[intRobot].setPos(x,y,z);
+	}
+	
+	public void ajouterOrdre (Ordre newOrdre)
+	{
+		this.stockOrdres.add(newOrdre);
+	}
+	
+	public ArrayList<Pion> getListePions()
+	{
+		this.metier.getListePions;
 	}
 }
